ffa7d303011064b066a3fa7fce6a5ef115c41bd5
khoingt <khoint828@proton.me>
2026-07-09 22:51:07 +0700

    refactor(ThreadingService): delegate to Task.Backgroundable internally
    Replace ProgressManager.runProcess(run, progressWindow) with
    Task.Backgroundable.queue() so the work runs in IntelliJ's standard
    background task lifecycle with visible progress in the status bar.
    
    The public signature is unchanged so callers don't need to adapt.

--- tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/services/ThreadingService.java
+++ tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/services/ThreadingService.java
@@ -1,21 +1,20 @@
 package fi.helsinki.cs.tmc.intellij.services;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.diagnostic.Logger;
+import org.jetbrains.annotations.NotNull;
 
 public class ThreadingService {
 
-    private static final Logger logger = Logger.getInstance(ThreadingService.class);
-
     public void runWithNotification(
             final Runnable run, Project project, ProgressIndicator progressWindow) {
-        logger.debug("Processing runWithNotification. @ThreadingService");
 
-        ApplicationManager.getApplication()
-                .executeOnPooledThread(
-                        () -> ProgressManager.getInstance().runProcess(run, progressWindow));
+        new Task.Backgroundable(project, "Working...", true) {
+            @Override
+            public void run(@NotNull ProgressIndicator indicator) {
+                run.run();
+            }
+        }.queue();
     }
 }
