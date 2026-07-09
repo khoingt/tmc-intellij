31001b9274d37c96fda3ca95eada60b60bdbf7e6
khoingt <khoint828@proton.me>
2026-07-09 22:52:46 +0700

    chore: remove orphaned test ProgressWindow workaround
    This empty interface was created solely because Mockito 1.9.0 could
    not mock ThreadingService. ThreadingService's implementation now
    uses Task.Backgroundable and is no longer mocked in tests.

--- tmc-plugin-intellij/src/test/java/com/intellij/openapi/progress/util/ProgressWindow.java
+++
@@ -1,8 +0,0 @@
-package com.intellij.openapi.progress.util;
-
-/*
-This is a simple workaround for tests. For whatever reason Mockito
-fails at creating a mock of ThreadingService.
- */
-public interface ProgressWindow {
-}
