8a149b27e0cdad3cd44c909a7475c90d13cfeebb
khoingt <khoint828@proton.me>
2026-07-09 09:13:48 +0700

    feat: make Welcome screen action state-aware
    Add update() to TmcSettingsAction to change button text between
    "Get started with TMC" and "Open TMC exercises" depending on login
    state. Route actionPerformed() on Welcome screen to ExerciseListDialog
    (when logged in) or LoginDialog (when not). Keep menu/toolbar settings
    behavior unchanged via ActionPlaces.WELCOME_SCREEN filtering.

--- tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/actions/buttonactions/TmcSettingsAction.java
+++ tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/actions/buttonactions/TmcSettingsAction.java
@@ -1,28 +1,47 @@
 package fi.helsinki.cs.tmc.intellij.actions.buttonactions;
 
+import com.intellij.openapi.actionSystem.ActionPlaces;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.diagnostic.Logger;
 import fi.helsinki.cs.tmc.intellij.holders.TmcSettingsManager;
+import fi.helsinki.cs.tmc.intellij.ui.exerciselist.ExerciseListDialog;
 import fi.helsinki.cs.tmc.intellij.ui.login.LoginDialog;
 import fi.helsinki.cs.tmc.intellij.ui.settings.SettingsWindow;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-/**
- * Opens the settings window. Defined in plugin.xml on line &lt;action id="Settings"
- * class="fi.helsinki.cs.tmc.intellij .actions.buttonactions.TmcSettingsAction"&gt;
- */
 public class TmcSettingsAction extends AnAction {
 
-    private static final Logger logger = LoggerFactory.getLogger(TmcSettingsAction.class);
+    private static final Logger logger = Logger.getInstance(TmcSettingsAction.class);
     private SettingsWindow window;
+
+    @Override
+    public void update(AnActionEvent e) {
+        if (ActionPlaces.WELCOME_SCREEN.equals(e.getPlace())) {
+            Presentation p = e.getPresentation();
+            boolean loggedIn = TmcSettingsManager.get().getToken().isPresent();
+            if (loggedIn) {
+                p.setText("Open TMC exercises");
+                p.setDescription("Browse and download TMC coursework exercises");
+            } else {
+                p.setText("Get started with TMC");
+                p.setDescription("Set up TMC to start working on coursework");
+            }
+        }
+    }
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
         logger.info("Performing TmcSettingsAction. @TmcSettingsAction");
-        showSettings();
+        if (ActionPlaces.WELCOME_SCREEN.equals(anActionEvent.getPlace())) {
+            if (TmcSettingsManager.get().getToken().isPresent()) {
+                ExerciseListDialog.display();
+            } else {
+                LoginDialog.display();
+            }
+        } else {
+            showSettings();
+        }
     }
 
     public void showSettings() {
